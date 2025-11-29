d84124612499dce8a349326c01c836c6956d65e3
Honório <150280168+Honorio15@users.noreply.github.com>
2025-11-29 01:35:43 -0300

    Add Robot class with main method

---
+++ Robot.java
@@ -0,0 +1,7 @@
+// Robot.java
+public class Robot {
+    public static void main(String[] args) {
+        System.out.println("🤖 Robô do Hilton funcionando!");
+        System.out.println("Java Version: " + System.getProperty("java.version"));
+    }
+}
